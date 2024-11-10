fa603163b9cd14b1a8eddc234f9eed7ea158cee3
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-10 11:03:50 +0600

    footer added and input field also

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
-    <div className="w-10/12 mx-auto">
-      <Navbar></Navbar>
+    <div className="w-10/12 mx-auto min-h-screen flex flex-col">
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <main className="flex-grow">
       <Outlet></Outlet>
+      </main>
+      <footer className="">
+      <Footer></Footer>
+      </footer>
     </div>
   );
 }
